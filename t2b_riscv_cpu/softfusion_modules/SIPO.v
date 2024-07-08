43beac8c1df30317059bce0b4e98bcd4ab10bc05
gaurav136 <gauravtale136@gmail.com>
2024-07-09 04:13:36 +0530

    mem_dev sub_mem working perfect and rd_addr index start from 1

--- t2b_riscv_cpu/softfusion_modules/SIPO.v
+++ t2b_riscv_cpu/softfusion_modules/SIPO.v
@@ -1,43 +1,3 @@
-// module SIPO (
-//     input wire clk,         // Clock input
-//     input wire reset,       // Reset input
-//     input wire done,
-//     input wire [31:0] SI,   // Serial input (32-bit wide)
-//     output reg [31:0] PO0,  // Parallel output 0 (32-bit wide)
-//     output reg [31:0] PO1,  // Parallel output 1 (32-bit wide)
-//     output reg [31:0] PO2,  // Parallel output 2 (32-bit wide)
-//     output reg [31:0] PO3,  // Parallel output 3 (32-bit wide)
-//     output reg [31:0] PO4,  // Parallel output 4 (32-bit wide)
-//     output reg [31:0] PO5   // Parallel output 5 (32-bit wide)
-// );
-
-//     reg [31:0] shift_register [0:5]; // 32-bit wide, 6-deep shift register
-
-//     integer i;
-
-//     always @(posedge clk or posedge reset) begin
-//         if (!reset) begin
-//             for (i = 0; i < 6; i = i + 1) begin
-//                 shift_register[i] <= 0; // Reset all stages to 0
-//             end
-//         end else begin
-//             for (i = 5; i > 0; i = i - 1) begin
-//                 shift_register[i] <= shift_register[i-1]; // Shift data down
-//             end
-//             shift_register[0] <= SI; // Load new data into first stage
-//         end
-//     end
-
-//     always @(posedge done) begin
-//         PO0 = shift_register[0];
-//         PO1 = shift_register[1];
-//         PO2 = shift_register[2];
-//         PO3 = shift_register[3];
-//         PO4 = shift_register[4];
-//         PO5 = shift_register[5];
-//     end
-
-// endmodule
 
 module SIPO (input wire clk,         // Clock input
              input wire reset,       // Reset input
@@ -71,7 +31,7 @@ module SIPO (input wire clk,         // Clock input
                 sipo_done <= 0;
             end
             end else begin
-
+            
             for (i = 0; i < 4; i = i + 1) begin
                 shift_register[i] <= 0; // Reset all stages to 0
             end
--- t2b_riscv_cpu/softfusion_modules/mem_div.v
+++ t2b_riscv_cpu/softfusion_modules/mem_div.v
@@ -1,98 +1,3 @@
-//module mem_div #(parameter DATA_WIDTH = 32,
-//                 ADDR_WIDTH = 32,
-//                 MEM_SIZE = 128)
-//                (input clk,
-//                 input CPU_DONE,
-//                 input [DATA_WIDTH-1:0] wr_data,
-//                 output [ADDR_WIDTH-1:0] read_addr,
-//                 output [DATA_WIDTH-1:0] rd_sub_mem0,
-//                 output [DATA_WIDTH-1:0] rd_sub_mem1,
-//                 output [DATA_WIDTH-1:0] rd_sub_mem2,
-//                 output [DATA_WIDTH-1:0] rd_sub_mem3);
-//    
-//    reg [3:0] sub_mem_wr_en;
-//    reg [ADDR_WIDTH-1:0] sub_mem_wr_addr;
-//    wire sipo_done;
-//    wire [DATA_WIDTH-1:0] sipo_out[0:3];
-//    reg [ADDR_WIDTH-3:0] rd_addr;
-//    
-//    // SIPO instance
-//    SIPO sipo_inst (
-//    .clk(clk),
-//    .reset(CPU_DONE),
-//    .SI(wr_data),
-//    .sipo_done(sipo_done),
-//    .PO0(sipo_out[0]),
-//    .PO1(sipo_out[1]),
-//    .PO2(sipo_out[2]),
-//    .PO3(sipo_out[3])
-//    );
-//    
-//    // Reset the write enable signals and address register on power-up
-//    initial begin
-//        sub_mem_wr_en   = 4'b0;
-//        sub_mem_wr_addr = 32'b0;
-//        rd_addr         = 32'd4;
-//		  read_addr 		= 32'b0;
-//    end
-//    
-//    assign read_addr = {rd_addr, 2'b00};
-//
-//    always @(posedge clk) begin
-//        if (CPU_DONE && rd_addr < 30'd124) begin
-//            rd_addr <= rd_addr + 30'd1;
-//        end
-//        else begin
-//            rd_addr <= 32'bx;
-//        end
-//    end
-//    
-//    
-//    always @(posedge clk) begin
-//        if (sipo_done) begin
-//            // Distribute data to sub-memories and update address
-//            sub_mem_wr_addr <= sub_mem_wr_addr + 1;
-//            sub_mem_wr_en   <= 4'b1111;
-//            end else begin
-//            sub_mem_wr_en <= 4'b0;
-//        end
-//    end
-//    
-//    // Instantiate the sub memories
-//    sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory0 (
-//    .clk(clk),
-//    .wr_en(sub_mem_wr_en[0]),
-//    .wr_addr(sub_mem_wr_addr),
-//    .wr_data(sipo_out[0]),
-//    .rd_sub_mem(rd_sub_mem0)
-//    );
-//    
-//    sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory1 (
-//    .clk(clk),
-//    .wr_en(sub_mem_wr_en[1]),
-//    .wr_addr(sub_mem_wr_addr),
-//    .wr_data(sipo_out[1]),
-//    .rd_sub_mem(rd_sub_mem1)
-//    );
-//    
-//    sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory2 (
-//    .clk(clk),
-//    .wr_en(sub_mem_wr_en[2]),
-//    .wr_addr(sub_mem_wr_addr),
-//    .wr_data(sipo_out[2]),
-//    .rd_sub_mem(rd_sub_mem2)
-//    );
-//    
-//    sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory3 (
-//    .clk(clk),
-//    .wr_en(sub_mem_wr_en[3]),
-//    .wr_addr(sub_mem_wr_addr),
-//    .wr_data(sipo_out[3]),
-//    .rd_sub_mem(rd_sub_mem3)
-//    );
-//    
-//endmodule
-
 
 module mem_div #(parameter DATA_WIDTH = 32,
                  ADDR_WIDTH = 32,
@@ -111,78 +16,87 @@ module mem_div #(parameter DATA_WIDTH = 32,
     wire sipo_done;
     wire [DATA_WIDTH-1:0] sipo_out[0:3];
     reg [ADDR_WIDTH-3:0] rd_addr;
-    
+    reg map_full;
     // SIPO instance
     SIPO sipo_inst (
-        .clk(clk),
-        .reset(CPU_DONE),
-        .SI(wr_data),
-        .sipo_done(sipo_done),
-        .PO0(sipo_out[0]),
-        .PO1(sipo_out[1]),
-        .PO2(sipo_out[2]),
-        .PO3(sipo_out[3])
+    .clk(clk),
+    .reset(CPU_DONE),
+    .SI(wr_data),
+    .sipo_done(sipo_done),
+    .PO0(sipo_out[0]),
+    .PO1(sipo_out[1]),
+    .PO2(sipo_out[2]),
+    .PO3(sipo_out[3])
     );
     
     // Reset the write enable signals and address register on power-up
     initial begin
-        sub_mem_wr_en = 4'b0;
+        sub_mem_wr_en   = 4'b0;
         sub_mem_wr_addr = 32'b0;
-        rd_addr = 29'd0;
+        rd_addr         = 29'd0;
+        map_full        = 0;
     end
     
     assign read_addr = {rd_addr, 2'b00};
-
+    
     always @(posedge clk) begin
-        if (CPU_DONE && rd_addr < 124) begin
-            rd_addr <= rd_addr + 29'd1;
-        end else begin
-            rd_addr <= 0; // Or some other default value
+        
+        if (CPU_DONE && rd_addr < 29'd127) begin
+            rd_addr  <= rd_addr + 29'd1;
+            map_full <= 1;
+            end else begin
+            rd_addr  <= 0; // Or some other default value
+            map_full <= 0;
         end
     end
     
     always @(posedge clk) begin
-        if (sipo_done) begin
-            // Distribute data to sub-memories and update address
-            sub_mem_wr_addr <= sub_mem_wr_addr + 1;
-            sub_mem_wr_en <= 4'b1111;
-        end else begin
+        if (map_full) begin
+            if (sipo_done) begin
+                // Distribute data to sub-memories and update address
+                sub_mem_wr_addr <= sub_mem_wr_addr + 1;
+                sub_mem_wr_en   <= 4'b1111;
+                end else begin
+                sub_mem_wr_en <= 4'b0;
+            end
+        end
+        else begin
             sub_mem_wr_en <= 4'b0;
         end
     end
     
     // Instantiate the sub memories
     sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory0 (
-        .clk(clk),
-        .wr_en(sub_mem_wr_en[0]),
-        .wr_addr(sub_mem_wr_addr),
-        .wr_data(sipo_out[0]),
-        .rd_sub_mem(rd_sub_mem0)
+    .clk(clk),
+    .wr_en(sub_mem_wr_en[0]),
+    .wr_addr(sub_mem_wr_addr),
+    .wr_data(sipo_out[0]),
+    .rd_sub_mem(rd_sub_mem0)
     );
-
+    
     sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory1 (
-        .clk(clk),
-        .wr_en(sub_mem_wr_en[1]),
-        .wr_addr(sub_mem_wr_addr),
-        .wr_data(sipo_out[1]),
-        .rd_sub_mem(rd_sub_mem1)
+    .clk(clk),
+    .wr_en(sub_mem_wr_en[1]),
+    .wr_addr(sub_mem_wr_addr),
+    .wr_data(sipo_out[1]),
+    .rd_sub_mem(rd_sub_mem1)
     );
-
+    
     sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory2 (
-        .clk(clk),
-        .wr_en(sub_mem_wr_en[2]),
-        .wr_addr(sub_mem_wr_addr),
-        .wr_data(sipo_out[2]),
-        .rd_sub_mem(rd_sub_mem2)
+    .clk(clk),
+    .wr_en(sub_mem_wr_en[2]),
+    .wr_addr(sub_mem_wr_addr),
+    .wr_data(sipo_out[2]),
+    .rd_sub_mem(rd_sub_mem2)
     );
-
+    
     sub_mem #(.DATA_WIDTH(DATA_WIDTH), .ADDR_WIDTH(ADDR_WIDTH), .MEM_SIZE(32)) sub_memory3 (
-        .clk(clk),
-        .wr_en(sub_mem_wr_en[3]),
-        .wr_addr(sub_mem_wr_addr),
-        .wr_data(sipo_out[3]),
-        .rd_sub_mem(rd_sub_mem3)
+    .clk(clk),
+    .wr_en(sub_mem_wr_en[3]),
+    .wr_addr(sub_mem_wr_addr),
+    .wr_data(sipo_out[3]),
+    .rd_sub_mem(rd_sub_mem3)
     );
-
+    
 endmodule
-
+    
